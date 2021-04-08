6b7a950bbc59513e51d024f012b247aeec8672de
Tinatin-art <omurovatinatin@gmail.com>
2021-04-09 00:31:21 +0600

    urok 30

--- Front-end/Javascript/urok28/scripts.js
+++ Front-end/Javascript/urok28/scripts.js
@@ -1,56 +1,100 @@
-function oper() {
-  var num1 = parseInt(document.getElementById('num1').value);
-  var num2 = parseInt(document.getElementById('num2').value);
-  var operators = document.getElementById('operators').value;
+// function oper() {
+//   var num1 = parseInt(document.getElementById('num1').value);
+//   var num2 = parseInt(document.getElementById('num2').value);
+//   var operators = document.getElementById('operators').value;
 
-  if (operators === '+') {
-    document.getElementById('result').value = num1 + num2;
-  }else if (operators === '-') {
-    document.getElementById('result').value = num1 - num2;
-  }else if (operators === '*') {
-    document.getElementById('result').value = num1 * num2;
-  }else if (operators === '/') {
-    document.getElementById('result').value = num1 / num2;
-  }else if (operators === '%') {
-    document.getElementById('result').value = num1 % num2;
-  }else if (operators === '**') {
-    document.getElementById('result').value = num1 ** num2;
-  }
-}
-var num1 = prompt("Введите первый слагаемое");
-var num2 = prompt("Введите второй слагаемое");
-alert("Сумма: " + (Number(num1) + Number(num2)));
+//   if (operators === '+') {
+//     document.getElementById('result').value = num1 + num2;
+//   }else if (operators === '-') {
+//     document.getElementById('result').value = num1 - num2;
+//   }else if (operators === '*') {
+//     document.getElementById('result').value = num1 * num2;
+//   }else if (operators === '/') {
+//     document.getElementById('result').value = num1 / num2;
+//   }else if (operators === '%') {
+//     document.getElementById('result').value = num1 % num2;
+//   }else if (operators === '**') {
+//     document.getElementById('result').value = num1 ** num2;
+//   }
+// }
+// var num1 = prompt("Введите первый слагаемое");
+// var num2 = prompt("Введите второй слагаемое");
+// alert("Сумма: " + (Number(num1) + Number(num2)));
 
-var num1 = prompt("Введите первый Уменьшаемое");
-var num2 = prompt("Введите второй Вычитаемое");
-alert("Разность: " + (Number(num1) - Number(num2)));
+// var num1 = prompt("Введите первый Уменьшаемое");
+// var num2 = prompt("Введите второй Вычитаемое");
+// alert("Разность: " + (Number(num1) - Number(num2)));
 
-var num1 = prompt("Введите первый множитель");
-var num2 = prompt("Введите второй множитель");
-alert("Произведение: " + (Number(num1) * Number(num2)));
+// var num1 = prompt("Введите первый множитель");
+// var num2 = prompt("Введите второй множитель");
+// alert("Произведение: " + (Number(num1) * Number(num2)));
 
-var num1 = prompt("Введите первый делимое");
-var num2 = prompt("Введите второй делитель");
-alert("Частное: " + (Number(num1) / Number(num2)));
+// var num1 = prompt("Введите первый делимое");
+// var num2 = prompt("Введите второй делитель");
+// alert("Частное: " + (Number(num1) / Number(num2)));
 
-var num1 = prompt("Введите первый делимое");
-var num2 = prompt("Введите второй делитель");
-alert("Остаток от деления: " + (Number(num1) % Number(num2)));
+// var num1 = prompt("Введите первый делимое");
+// var num2 = prompt("Введите второй делитель");
+// alert("Остаток от деления: " + (Number(num1) % Number(num2)));
 
-var num1 = prompt("Введите первое число");
-var num2 = prompt("Введите второе число");
-alert("Возведение в степень: " + (Number(num1) ** Number(num2)));
+// var num1 = prompt("Введите первое число");
+// var num2 = prompt("Введите второе число");
+// alert("Возведение в степень: " + (Number(num1) ** Number(num2)));
 
-var parol = prompt("Введите пароль: ", "");
-if(parol === "hello"){
-  console.log("Wellcome");
-}else{
-  console.log("пароль не правильный");
-}
+// var parol = prompt("Введите пароль: ", "");
+// if(parol === "hello"){
+//   console.log("Wellcome");
+// }else{
+//   console.log("пароль не правильный");
+// }
 
-var login = prompt("Введите логин: ", "");
-if(login === "java"){
-  console.log("Wellcome");
-}else{
-  console.log("логин не правильный");
-}
+// var login = prompt("Введите логин: ", "");
+// if(login === "java"){
+//   console.log("Wellcome");
+// }else{
+//   console.log("логин не правильный");
+// }
+
+
+
+// Можно было так решить
+
+
+// var first = +prompt('Enter the first number');
+// var second = +prompt('Enter the second number');
+// var operator = prompt('Enter operator');
+
+// if (operator == '-') {
+//   console.log(first - second);
+// }
+// if (operator == '+') {
+//   console.log(first + second);
+// }
+// if (operator == '*') {
+//   console.log(first * second);
+// }
+// if (operator == '/') {
+//   console.log(first / second);
+// }
+// if (operator == '%') {
+//   console.log(first % second);
+// }
+// if (operator == '**') {
+//   console.log(first ** second);
+// }
+
+// var login = prompt('Enter your login');
+// var password = prompt('Enter your password');
+
+// if (login == 'admin' && password == 'pass'){
+//   console.log('Wellcome');
+// }
+// else if (login != 'admin'){
+//   console.log('Login is uncorrect');
+// }
+// else if (password != 'pass'){
+//   console.log('password is uncorrect');
+// }
+
+
+// console.log(null || undefined || false || true);
