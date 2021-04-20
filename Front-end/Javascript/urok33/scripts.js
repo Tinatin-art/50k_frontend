50acece775e1763c64f71ab7b406ab1c4c575583
Tinatin-art <omurovatinatin@gmail.com>
2021-04-20 16:08:29 +0600

    dz urok 33

--- Front-end/Javascript/urok33/scripts.js
+++ Front-end/Javascript/urok33/scripts.js
@@ -1,2 +1,29 @@
 // Д.з 33 урока
 
+// 1. Сделать callback функцию которая просит ввести логин и пароль, и проверяет правильно ли введены данные пользователя 
+
+function user(login, password, callback) {
+  if (login == 'admin' && password == 'parol' ){
+    alert('Добро пожаловать! Вы правильно ввели все данные');
+  }else if (login == 'admin' || password == 'parol' ){
+    alert('Вы неправильно ввели логин или пароль. Попробуйте еще раз!');
+  }else {
+    callback();
+  }
+}
+
+user( login = prompt('Ведите логин: ', ''), password = prompt('Введите пароль: ', ''), () => {
+  alert("Извините, все данные неправильные. Попробйте еще раз!");
+});
+
+// 2. Сделать callback функцию которая создаёт html элемент, то есть функция просит какой тег создать, просит какой текст ввести и цвет текста. 
+
+function addElement(){
+  var newh2 = document.createElement("h2");
+  newh2.innerText = "I am a newh2";
+  newh2.style.color = "red";
+
+  document.body.appendChild(newh2);
+}
+
+addElement();
