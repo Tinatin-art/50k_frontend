26d5dcf22de4d046cf1027b12638ec48a6720bb6
Tinatin-art <omurovatinatin@gmail.com>
2021-04-21 22:31:19 +0600

    dz urok 34

--- Front-end/Javascript/urok34/scripts.js
+++ Front-end/Javascript/urok34/scripts.js
@@ -1,1 +1,26 @@
-// Д.з 33 урока
+// Д.з 34 урока
+
+// дан объект person = {
+// 	name: 'adilet',
+// 	national: 'kyrgyz',
+// 	age: 23,
+// 	year: 1996,
+// };
+// с помощью деструктуризация сделайте такой вывод 
+//  output:  name = ‘adilet’, national = ‘kyrgyz’, age = 23, year = 1996, все это не объект а отдельные переменные которые не относятся к объекту 
+
+let persona = {
+  	name: 'adilet',
+  	national: 'kyrgyz',
+  	age: 23,
+  	year: 1996,
+  };
+
+console.log(persona);
+
+let array2 = [];
+
+for (i = 0; i < persona.length; i++){
+  array2.push(persona[i]);
+  console.log(persona[i]);
+}
