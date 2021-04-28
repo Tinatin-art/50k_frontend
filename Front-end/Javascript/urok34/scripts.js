b00bf8eacc38490f4fab6f0dc324bb31eaa98fe0
Tinatin-art <omurovatinatin@gmail.com>
2021-04-28 21:13:41 +0600

    dz urok 34

--- Front-end/Javascript/urok34/scripts.js
+++ Front-end/Javascript/urok34/scripts.js
@@ -1,26 +1,15 @@
 // Д.з 34 урока
 
-// дан объект person = {
-// 	name: 'adilet',
-// 	national: 'kyrgyz',
-// 	age: 23,
-// 	year: 1996,
-// };
-// с помощью деструктуризация сделайте такой вывод 
-//  output:  name = ‘adilet’, national = ‘kyrgyz’, age = 23, year = 1996, все это не объект а отдельные переменные которые не относятся к объекту 
+// 1. Преобразуйте псевдо массив  в массив
+// a) дан псевдо массив  {0: 'first', 1: 'second', 2: 'third', length: 3} 
+//                        вывод [ "first", "second", "third" ]
+// b) дан массив ["first", "second", "third" ]
+// 		  вывод [ "f", "i", "r", "s", "t" ], [ "s", "e", "c", "o", "n", "d" ] [ "t", "h", "i", "r", "d" ] 
 
-let persona = {
-  	name: 'adilet',
-  	national: 'kyrgyz',
-  	age: 23,
-  	year: 1996,
-  };
+let arr = {0: 'first', 1: 'second', 2: 'third', length: 3};
+console.log(Array.from(arr));
 
-console.log(persona);
+let arr2 = ["first", "second", "third" ];
+let str = arr2.join('');
+console.log(str.split(""));
 
-let array2 = [];
-
-for (i = 0; i < persona.length; i++){
-  array2.push(persona[i]);
-  console.log(persona[i]);
-}
