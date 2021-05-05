708ad5d909ec6c378dee33590eaba94d7d780407
Tinatin-art <omurovatinatin@gmail.com>
2021-05-06 00:58:08 +0600

    urok 38

---
+++ Front-end/Javascript/urok38/scripts.js
@@ -0,0 +1,15 @@
+// Урок 38
+// Динамическая типизация
+
+// let str = 'text';
+// let strObj = String(str);
+// console.log(typeof(str));
+// console.log(strObj);
+
+// let number = "1";
+// console.log(+number + 5);
+// console.log(parseInt(number) + 5);
+// let res = +prompt("Enter number", "");
+// console.log(typeof(res));
+// console.log("https://miukid.com/cat/" +res);
+// console.log(`https://miukid.com/cat/"${res}`);
